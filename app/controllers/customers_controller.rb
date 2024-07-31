93e21976abd5642f946b0aac705b9c4be5a4b16a
akshaybabar <akshaybabar2408@gmail.com>
2024-07-31 13:02:07 +0530

    Add endpoint for file upload and processing in CustomersController

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,12 @@
+# app/controllers/customers_controller.rb
 class CustomersController < ApplicationController
   def index
+    if params[:file].present?
+      file = params[:file].read
+      @customers = CustomerInviter.call(file)
+      render json: @customers, status: :ok
+    else
+      render json: { error: 'File not provided' }, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  get 'customers/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :customers, only: [:index]
 end
